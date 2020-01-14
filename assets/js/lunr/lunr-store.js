a47fb2160e64495865ec99468311531fb026e50f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-14 21:21:19 +0000

    Deploy kejace/hs-abci to github.com/kejace/hs-abci.git:gh-pages

--- assets/js/lunr/lunr-store.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "hello world",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "/hs-abci/2020/01/07/first-post.html",
-        "teaser":null}]
